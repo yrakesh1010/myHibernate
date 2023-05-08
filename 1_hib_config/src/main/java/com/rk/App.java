a649cce1a5f13e334e554ddac06f71ef216425c2
rakeshy4 <rakeshy4@BSHYDL-022253.BIRLASOFT.COM>
2023-05-08 14:10:32 +0530

    Add student address with image

--- 1_hib_config/src/main/java/com/rk/App.java
+++ 1_hib_config/src/main/java/com/rk/App.java
@@ -1,19 +1,21 @@
 package com.rk;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.Date;
+
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.Transaction;
 import org.hibernate.cfg.Configuration;
 
+import com.entity.Address;
 import com.entity.Student;
 
-/**
- * Hello world!
- *
- */
 public class App 
 {
-    public static void main( String[] args )
+    public static void main( String[] args ) throws IOException
     {
     	System.out.println( "Programm Started..." );
     	
@@ -26,6 +28,10 @@ public class App
         Student stu=null;
         stu=new Student();
         
+        //Address object
+        Address addr=null; 
+        addr = new Address();
+        
         SessionFactory factory=cfg.buildSessionFactory();
     	
         System.out.println("Factory object"+factory);
@@ -33,15 +39,30 @@ public class App
         System.out.println(factory.isClosed());
         
         //set student value
-        stu.setId(101);
-        stu.setName("Jhon");
-        stu.setCity("Newyork");
+        stu.setId(102);
+        stu.setName("Adam");
+        stu.setCity("Australia");
+        
+        //set address value
+        addr.setStreet("street1");
+        addr.setCity("Londan");
+        addr.setIsopen(true);
+        addr.setAddedDate(new Date());
+        addr.setX(123.373);
+        
+        //set Image
+        FileInputStream file=new FileInputStream("src\\main\\java\\156662.jpg");
+        byte[] img=new byte[file.available()];
+        addr.setImage(img);
         
         Session currentSession = factory.openSession();
         Transaction beginTransaction = currentSession.beginTransaction();
         currentSession.save(stu);
+        currentSession.save(addr);
         beginTransaction.commit();
         currentSession.close();
+        
+        System.out.println("Task complete....");
         
         
         
