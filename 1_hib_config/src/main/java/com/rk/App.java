0b91e977f4bd816f9888e92f7013431bfdfb5dda
rakeshy4 <rakeshy4@BSHYDL-022253.BIRLASOFT.COM>
2023-05-08 13:10:58 +0530

    Add entity class

--- 1_hib_config/src/main/java/com/rk/App.java
+++ 1_hib_config/src/main/java/com/rk/App.java
@@ -1,7 +1,11 @@
 package com.rk;
 
+import org.hibernate.Session;
 import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
 import org.hibernate.cfg.Configuration;
+
+import com.entity.Student;
 
 /**
  * Hello world!
@@ -13,14 +17,38 @@ public class App
     {
     	System.out.println( "Programm Started..." );
     	
+    	//hibernats configuration
     	Configuration cfg=null;
         cfg=new Configuration();
         cfg.configure("hibernate.cfg.xml");
+        
+        //Student object
+        Student stu=null;
+        stu=new Student();
         
         SessionFactory factory=cfg.buildSessionFactory();
     	
         System.out.println("Factory object"+factory);
     	
         System.out.println(factory.isClosed());
+        
+        //set student value
+        stu.setId(101);
+        stu.setName("Jhon");
+        stu.setCity("Newyork");
+        
+        Session currentSession = factory.openSession();
+        Transaction beginTransaction = currentSession.beginTransaction();
+        currentSession.save(stu);
+        beginTransaction.commit();
+        currentSession.close();
+        
+        
+        
+        
+        
+        
+        
+        
     }
 }
