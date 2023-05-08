c4b37d4dcca5377bffcdc623e9103ce1cd0dd2c7
rakeshy4 <rakeshy4@BSHYDL-022253.BIRLASOFT.COM>
2023-05-08 11:18:52 +0530

    Change in Main class

--- 1_hib_config/src/main/java/com/rk/App.java
+++ 1_hib_config/src/main/java/com/rk/App.java
@@ -1,5 +1,8 @@
 package com.rk;
 
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
 /**
  * Hello world!
  *
@@ -8,6 +11,16 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+    	System.out.println( "Programm Started..." );
+    	
+    	Configuration cfg=null;
+        cfg=new Configuration();
+        cfg.configure("hibernate.cfg.xml");
+        
+        SessionFactory factory=cfg.buildSessionFactory();
+    	
+        System.out.println("Factory object"+factory);
+    	
+        System.out.println(factory.isClosed());
     }
 }
