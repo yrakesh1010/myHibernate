68f6e570e6cae3f216230ccdbcbe079bfbc61ee8
rakeshy4 <rakeshy4@BSHYDL-022253.BIRLASOFT.COM>
2023-05-10 11:44:15 +0530

    HQl inner join

---
+++ 1_hib_config/src/main/java/com/rk/HqlApp_2_joins.java
@@ -0,0 +1,33 @@
+package com.rk;
+
+import java.util.Arrays;
+import java.util.List;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.cfg.Configuration;
+import org.hibernate.query.Query;
+
+public class HqlApp_2_joins {
+
+	public static void main(String[] args) {
+		SessionFactory factory=new Configuration().configure("hibernate.cfg.xml").buildSessionFactory();
+		
+		Session openSession = factory.openSession();
+		Transaction txt = openSession.beginTransaction();
+		
+		String joinQuery = "select e.questionId,e.question, a.answer from Question e INNER JOIN e.answer a";
+        Query createQuery = openSession.createQuery(joinQuery);
+        List<Object[]> resultList = createQuery.getResultList();
+        for(Object[] obj:resultList) {
+        	System.out.println(Arrays.toString(obj));
+        }
+        
+        txt.commit();
+        openSession.close();
+        factory.close();
+        
+	}
+
+}
